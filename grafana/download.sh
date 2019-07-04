4443e5e8b43707ae88bdf044b8af45200f715fdf
Mark Ebert <markebert@users.noreply.github.com>
2019-07-04 01:03:54 -0400

    Update

--- grafana/download.sh
+++ grafana/download.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 if [ "$1" = "raspberry-pi" ]; then
-	curl -o /tmp/grafana.deb https://dl.grafana.com/oss/release/grafana-rpi_6.1.6_armhf.deb;
+	curl -o /tmp/grafana.deb https://dl.grafana.com/oss/release/grafana-rpi_6.2.5_armhf.deb
 else
-	curl -o /tmp/grafana.deb https://dl.grafana.com/oss/release/grafana_6.1.6_armhf.deb;
+	curl -o /tmp/grafana.deb https://dl.grafana.com/oss/release/grafana_6.2.5_armhf.deb 
 fi
